772580c4932be203605bc498d905ae6ab933c79e
Mardonio Silva da Costa <mrdonio.sc@gmail.com>
2020-02-03 10:49:57 -0300

    Feito o cadastro de pessoa juridica com validadores de dados no formulário

--- src/app/autenticacao/cadastro-pj/cadastro-pj.module.ts
+++ src/app/autenticacao/cadastro-pj/cadastro-pj.module.ts
@@ -3,8 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { RouterModule } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http'
+
+import {
+  MatInputModule,
+  MatButtonModule,
+  MatListModule,
+  MatTooltipModule,
+  MatIconModule,
+  MatSnackBarModule
+} from '@angular/material'
 
 import { CadastrarPjComponent, CadastroPjComponent } from './components';
+
+import { SharedModule } from '../../shared/shared.module'
 
 @NgModule({
   declarations: [
@@ -14,7 +27,16 @@ import { CadastrarPjComponent, CadastroPjComponent } from './components';
   imports: [
     CommonModule,
     RouterModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatSnackBarModule,
+    SharedModule
   ]
 })
 export class CadastroPjModule { }
